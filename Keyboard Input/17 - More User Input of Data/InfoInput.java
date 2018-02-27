33b2a3884ea2565b064dd20fa4c8448c44fafbe5
Zero97M <36473963+Zero97M@users.noreply.github.com>
2018-02-27 12:46:20 +0530

    Created InfoInput.java

---
+++ Keyboard Input/17 - More User Input of Data/InfoInput.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class InfoInput
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		String firstName, lastName, login;
+		int grade, studentID;
+		double gpa;
+		
+		System.out.println("Please enter the following information so I can sell it for a profit!");
+		
+		System.out.println(" ");
+
+		System.out.print("First name: ");
+		firstName = keyboard.next();
+
+		System.out.print("Last name: ");
+		lastName = keyboard.next();
+
+		System.out.print("Grade (9-12) : ");
+		grade = keyboard.nextInt();
+
+		System.out.print("Student ID: ");
+		studentID = keyboard.nextInt();
+
+		System.out.print("Login: ");
+		login = keyboard.next();
+
+		System.out.print("GPA (0.0-4.0): ");
+		gpa = keyboard.nextDouble();
+
+		System.out.println(" ");
+		System.out.println("Your information: ");
+		System.out.println("		Login: " + login);
+		System.out.println("		ID:    " + studentID);
+		System.out.println("		Name:  " + lastName + ", " + firstName);
+		System.out.println("		GPA:   " + gpa);
+		System.out.println("		Grade: " + grade);
+
+	}
+}
