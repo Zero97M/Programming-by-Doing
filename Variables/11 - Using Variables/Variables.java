7a6d647618a19aef02dc76c75cda6cae4c6915bb
Zero97M <36473963+Zero97M@users.noreply.github.com>
2018-02-27 11:29:17 +0530

    Created Variables.java

---
+++ Variables/11 - Using Variables/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+       int room = 113;
+       double e = 2.71827;
+       String subject = "Computer Science";
+       
+        System.out.println("This is room # " + room);
+        System.out.println("e is close to " + e);
+        System.out.println("I am learning a bit about " + subject);
+    }
+    
+}
